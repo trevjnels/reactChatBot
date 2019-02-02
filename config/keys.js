54fd8faed21ae21ecbee20045457c0d2b0e1e665
trevjnels <trevorjohnnels@gmail.com>
2019-02-02 15:01:39 -0700

    still trying

--- config/keys.js
+++ config/keys.js
@@ -1,29 +1,35 @@
-if (process.env.NODE_ENV === "production") {
-  module.exports = {
-    googleProjectID: process.env.GOOGLE_PROJECT_ID,
-    dialogFlowSessionID: process.env.DIALOG_FLOW_SESSION_ID,
-    dialogFlorSessionLanguageCode:
-      process.env.DIALOG_FLOW_SESSION_LANGUAGE_CODE,
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY
-  };
+// prettier-ignore
+
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require("./prod")
 } else {
-  const dialogflow = require("./development.json");
-  const {
-    googleProjectID,
-    dialogFlowSessionID,
-    dialogFlowSessionLanguageCode,
-    private_key,
-    client_email
-  } = dialogflow;
-
-  module.exports = {
-    googleProjectID,
-    dialogFlowSessionID,
-    dialogFlowSessionLanguageCode,
-    private_key,
-    client_email
-  };
+  module.exports = require('./dev')
 }
-
-//in case this thing breaks, make a dialogflow_keys.json file with the reactpageagnet file in it
+//
+//
+// if (process.env.NODE_ENV === "production") {
+//   module.exports = {
+//     googleProjectID: process.env.GOOGLE_PROJECT_ID,
+//     dialogFlowSessionID: process.env.DIALOG_FLOW_SESSION_ID,
+//     dialogFlorSessionLanguageCode: process.env.DIALOG_FLOW_SESSION_LANGUAGE_CODE,
+//     client_email: process.env.GOOGLE_CLIENT_EMAIL,
+//     private_key: process.env.GOOGLE_PRIVATE_KEY
+//   };
+// } else {
+//   const keyObj = require("./development.json");
+//   const {
+//     googleProjectID,
+//     dialogFlowSessionID,
+//     dialogFlowSessionLanguageCode,
+//     private_key,
+//     client_email
+//   } = keyObj;
+//
+//   module.exports = {
+//     googleProjectID,
+//     dialogFlowSessionID,
+//     dialogFlowSessionLanguageCode,
+//     private_key,
+//     client_email
+//   };
+// }
